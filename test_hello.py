df8289fc7337e810f47f6d5d09735655d6607a33
yardenivry <107366071+yardenivry@users.noreply.github.com>
2022-12-08 19:58:47 +0200

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 from devopslib.fruit import fruit
 
 def test_fruit():
     fruit_choice = fruit()
     assert fruit_choice in ["apple", "cherry", "melon"]
-=======
-from devopslib.fruit import fruit
-
-def test_fruit():
-    fruit_choice = fruit()
-    assert fruit_choice in ["apple", "cherry", "melon"]
->>>>>>> 2adbad84b1a7e2a10e9e99053287335382354555
